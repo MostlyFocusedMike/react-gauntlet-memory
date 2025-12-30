5f59011ef14e0fcaad288f81b4e966fd43b0df56
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2025-12-29 21:34:32 -0500

    how can this be what grid is

--- src/App.tsx
+++ src/App.tsx
@@ -58,16 +58,17 @@ export default function App() {
       <h1 className="text-4xl p-2 font-bold">Memory Game</h1>
       <p>Paris Left: {remainingPairs !== 0 ? remainingPairs : 'Victory!'}</p>
       <p>Score: {score}</p>
-      <ul className="grid grid-cols-4">
+      <ul className="grid grid-cols-4 w-fit m-auto">
         {cards.map((guess) => {
           const { id, val, isMatched, isVisible } = guess;
 
           return (
             <li key={id}>
               <button
-                className={`border px-2 w-10 h-10 rounded  disabled:bg-emerald-400 ${isVisible ? 'bg-slate-100' : 'bg-slate-300'}`}
+                className={`border px-2 w-10 h-10 rounded  ${isVisible ? 'bg-slate-100' : 'bg-slate-300'} data-matched:bg-emerald-400`}
+                data-matched={isMatched || undefined}
                 onClick={() => checkGuess(id, guess)}
-                disabled={isMatched}
+                disabled={isVisible || isMatched}
               >
                 {isVisible || isMatched ? val : "?"}
               </button>
